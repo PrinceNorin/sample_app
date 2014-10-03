0297014d52b0a735a825f1f6b020c5b9698389cc
Hor Norin <norin.hor@gmail.com>
2014-10-03 15:34:43 +0700

    Finish add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   def feed
-    microposts
+    Micropost.from_users_followed_by(self)
   end
   
   def following?(other_user)
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -10,6 +10,7 @@ describe Micropost do
   
   it { should respond_to :content }
   it { should respond_to :user_id }
+  # it { should respond_to :from_users_followed_by }
   it { should be_valid }
   its(:user) { should eq user }
   
